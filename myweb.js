400ab85fa7d2ae70bca3b51426fb915375adc7df
chirag-ghoghari <162360866+chirag-ghoghari@users.noreply.github.com>
2024-08-28 18:11:47 +0530

    cmt

---
+++ myweb.js
@@ -0,0 +1,22 @@
+ // Get the modal
+      var modal = document.getElementById('id01');
+      
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+          if (event.target == modal) {
+              modal.style.display = "none";
+          }
+      }
+    //   $(document).ready(function(){
+    //   $(window).scroll(function(){
+    //     $('.main').css({
+    //       position: 'fixed',
+    //       background:'#a6edb1'
+    //      });
+    //   });
+    // });
+    $(document).ready(function(){
+        $("#back").click(function(){
+          $('.icon-set #tech-img').attr("src","web-img/shopify.png")8
+        });
+      });
